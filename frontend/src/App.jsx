cc90f7e39f41b1fbdfc070d0eb5a1de7c507b6f1
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-06 16:37:19 +0530

    chakra Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 
 function App() {
 
 
   return (
     <>
-    
+      <div>
+        <Button>LINK</Button>
+      </div>
+ 
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ChakraProvider } from "@chakra-ui/react"
 import App from './App.jsx'
+import system from "./theme";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
