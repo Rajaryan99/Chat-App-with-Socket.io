09b0f6f07fe1b6b99cd49f09ebf5a13519af6deb
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-06 10:54:52 +0530

    added backend server

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
