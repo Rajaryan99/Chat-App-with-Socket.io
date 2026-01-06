2768bdb8324a3e8af62d5156c0283113679c4294
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-06 15:28:11 +0530

    GETDATA endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,24 @@ import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import mongoose from 'mongoose';
+import {chats} from './data/data.js'
 
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
+
+app.get('/getdata', (req, res) => {
+    res.send(chats)
+})
+
+
 
 const connectDB = async () => {
     try {
