407e9071142085bcba2ba7a0f31906bd895967db
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-07 17:35:15 +0530

    login signup form

--- frontend/src/components/Authentication/LoginPage.jsx
+++ frontend/src/components/Authentication/LoginPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import { VStack } from '@chakra-ui/react'
+import './Auth.css'
 
 export default function LoginPage() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+   
+  
+   
+  
+    const submitHandler = () => {
+      
+    }
+    const guestHandler = () => {
+      
+    }
   return (
-    <div>
-      <h1>Log in</h1>
-    </div>
+     <VStack spacing='5px' >
+          <form action="" id='signup'>
+  
+            <label htmlFor="email">email: </label>
+            <input className='singupInput' type="email" placeholder='Enter Your email' name='email' required onClick={(e) => setEmail(e.target.value)} /> <br />
+     
+            <label htmlFor="password">Password: </label>
+            <input className='singupInput' type="password" placeholder='Enter Your password' name='password' required onClick={(e) => setPassword(e.target.value)} /><br /> <br />
+        
+          
+            <div id='signupDiv'>
+              <button id='signbtn' onClick={submitHandler}>LogIn</button>
+            </div>
+            <div id='signupDiv'>
+          <button id='guest' onClick={guestHandler}>Get guest user credential</button>
+            </div>
+          </form>
+        </VStack>
   )
 }
--- frontend/src/components/Authentication/SignupPage.jsx
+++ frontend/src/components/Authentication/SignupPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { VStack } from '@chakra-ui/react'
+import './Auth.css'
 
 export default function SignupPage() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [password, setPassword] = useState();
+  const [pic, setPic] = useState()
+
+  const postDetails = () => {
+
+  }
+
+  const submitHandler = () => {
+    
+  }
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <VStack spacing='5px' >
+      <form action="" id='signup'>
+        <label htmlFor="username">Name: </label>
+        <input className='singupInput' type="text" placeholder='Enter Your name' name='username' required  onClick={(e) => setName(e.target.value)}/> <br /><br />
+
+        <label htmlFor="email">email: </label>
+        <input className='singupInput' type="email" placeholder='Enter Your email' name='email' required onClick={(e) => setEmail(e.target.value)} /> <br /><br />
+ 
+        <label htmlFor="password">Password: </label>
+        <input className='singupInput' type="password" placeholder='Enter Your password' name='password' required onClick={(e) => setPassword(e.target.value)} /><br /> <br />
+    
+        <label htmlFor="Cpassword">Confirm Password: </label>
+        <input className='singupInput' type="password" placeholder='Confirm password' name='Cpassword' required onClick={(e) => setConfirmPassword(e.target.value)} /> <br /> <br />
+
+
+        <label htmlFor="dp">Upload your Picture </label>
+        <input className='pic' type="file" placeholder='Upload image' name='dp' accept='image/*' onChange={(e) => postDetails(e.target.files[0])} /> <br /> <br />
+
+
+
+        <div id='signupDiv'>
+          <button id='signbtn' onClick={submitHandler}>SignUP</button>
+        </div>
+      </form>
+    </VStack>
   )
 }
