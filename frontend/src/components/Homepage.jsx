6816370c57ca053d0f9f4bd0471948e3e3318fbb
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-07 13:27:52 +0530

    Auth page atyling

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import './Home.css'
+import { Box, Container, Text, Tabs } from '@chakra-ui/react'
+import Login  from './Authentication/LoginPage'
+import Signup from './Authentication/SignupPage'
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container maxW='xl'>
+      <Box d={'flex'}
+      // height={'40px'}
+        width={'100%'}
+        p={'5px'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        textAlign={'center'}
+        borderRadius={'10px'}
+        mt={'10px'}
+        className='box'
+      >
+        <Text className='title' fontSize={'4xl'} color={'black'}>Let's Talk</Text>
+      </Box>
+      <Box className='box' w={'100%'} padding={'2'} borderRadius={'10px'} mt={'10px'}
+        justifyContent={'center'}
+        alignItems={'center'}
+       
+      >
+
+        <Tabs.Root variant="line"   fitted defaultValue={"tab-1"}>
+          <Tabs.List>
+            <Tabs.Trigger fontSize={'18px'} value="login">Login</Tabs.Trigger>
+            <Tabs.Trigger fontSize={'18px'} value="signup">SignUp</Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="login">{ <Login/>}</Tabs.Content>
+          <Tabs.Content value="signup"><Signup/></Tabs.Content>
+          
+        </Tabs.Root>
+
+      </Box>
+    </Container>
   )
 }
