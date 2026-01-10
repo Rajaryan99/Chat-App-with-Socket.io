88c497c7e29584975b1ba7d7564702737a2febd4
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-10 20:26:41 +0530

    signup

--- frontend/src/components/Authentication/SignupPage.jsx
+++ frontend/src/components/Authentication/SignupPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { VStack } from '@chakra-ui/react'
 import './Auth.css'
+import axios from 'axios';
+import {useHistory} from 'react-router-dom'
+
 
 export default function SignupPage() {
 
@@ -9,14 +12,65 @@ export default function SignupPage() {
   const [confirmPassword, setConfirmPassword] = useState();
   const [password, setPassword] = useState();
   const [pic, setPic] = useState()
+  const [loading, setLoading] = useState(false)
+  const history = useHistory();
 
-  const postDetails = () => {
-
-  }
-
-  const submitHandler = () => {
+  const postDetails = (pics) => {
+    setLoadng(true);
+    if (pic === undefined) {
+      alert("Please select pictures ");
+      return;
+    }
     
-  }
+    if (pics.type === 'image/jpeg' || pics.type === 'image/png') {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("uploade preset", "chat-app")
+      fetch('cloudinary://285843744766645:hyI7OqQY3sEKnpX3gzduFPBcPyg@dke8bcv8q', {
+        method: 'post',
+        body: data,
+      }).then((res) => res.json())
+        .then(data => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false)
+        })
+      .catch
+    }
+    }
+  
+  const submitHandler = async () => {
+    if (!name || !email || !password || !confirmPassword) {
+      alert("please fill all the details")
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert("password doesnot match")
+      return;
+    }
+
+
+    try {
+
+      const config = {
+        headers: {
+          "content-type": "application/json"
+          
+        },
+      }
+
+      const { data } = await axios.post("/user", { name, email, password, pic }, config)
+      alert('Registeration successful');
+      localStorage.setItem('userInfo', JSON.stringify(data))
+      history.push('/chats')
+      
+    } catch (error) {
+      console.log(error)
+    }
+}
+  
+
   return (
     <VStack spacing='5px' >
       <form action="" id='signup'>
@@ -45,3 +99,4 @@ export default function SignupPage() {
     </VStack>
   )
 }
+
